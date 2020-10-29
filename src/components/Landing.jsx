81784c934c46a50942571ccd18e5aa8eb11fec1e
Dawid Mszanowski <dmszanowski@icloud.com>
2020-10-29 23:30:39 +0100

    Add Landing__Face

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -10,8 +10,9 @@ export const Landing = () => {
             <Col>
                 <Row>
                     <Col className="d-flex justify-content-center">
-                        <div className={classes.Landing__Header}>
-                            <h1>Welcome</h1>
+                        <div className={classes.Landing__Face}>
+                            <h1 className={classes.Landing__Face__Header}>Welcome</h1>
+                            <span className={classes.Landing__Face__Desc}>Consectetur lorem donec massa sapien faucibus et molestie ac feugiat. Massa massa ultricies mi quis hendrerit dolor magna. Nascetur ridiculus mus mauris vitae. Tellus mauris a diam maecenas sed. Laoreet suspendisse interdum consectetur libero id.</span>
                         </div>
                         <Button className={classes.Landing__Button}><i className="fas fa-arrow-down"/></Button>
                     </Col>
